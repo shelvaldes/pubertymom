e0978ca6a7278ae3997285c8f6151c7446512ab2
Shel Valdés <creative@shelvaldes.com>
2021-10-10 23:46:44 -0500

    Initial commit

---
+++ src/main/java/com/shelvaldes/pubertymom/PubertymomApplication.java
@@ -0,0 +1,13 @@
+package com.shelvaldes.pubertymom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubertymomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubertymomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shelvaldes/pubertymom/PubertymomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shelvaldes.pubertymom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubertymomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
