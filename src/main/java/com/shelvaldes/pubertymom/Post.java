530af15d8a8aa2c8d9476f7114ccb171c20f037c
Shel Valdés <creative@shelvaldes.com>
2021-10-13 00:50:02 -0500

    Conexión a base de datos local

---
+++ src/main/java/com/shelvaldes/pubertymom/Post.java
@@ -0,0 +1,12 @@
+package com.shelvaldes.pubertymom;
+
+public class Post {
+	private int id;
+	private String cambio;
+	private String accion;
+	private String emocion;
+	private String consejo;
+	
+	
+	
+}
